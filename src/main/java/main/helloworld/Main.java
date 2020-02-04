a0e474c0cfbb262f1c4b59540aa8c00df77bd123
Svjetlana Divljan <svjetlana.divljan@gmail.com>
2020-02-04 20:19:07 +0100

    Adds age cheker

--- src/main/java/main/helloworld/Main.java
+++ src/main/java/main/helloworld/Main.java
@@ -1,5 +1,7 @@
 package main.helloworld;
 
+import java.util.Scanner;
+
 /**
  *
  * @author qa
@@ -13,6 +15,42 @@ public class Main {
      System.out.println("Hello World!");
      System.out.println("Svjetlana Divljan");
      
+     int age =5;
+     
+     if(age > 18) {
+         System.out.println("Osoba je punoletna");
+     } else { 
+         System.out.println("Osoba je maloletna");
+     
+     }
+     System.out.println("Unesite broj godina:");
+     Scanner input = new Scanner(System.in);
+     int userAge = input.nextInt();
+     
+      System.out.println("Korisnik je uneo: " + userAge); 
+      
+     System.out.print("Unesite broj godina:");
+     Scanner input2 = new Scanner(System.in);
+     int personAge = input2.nextInt(); 
+     
+     if (personAge <=0 && personAge >150) {
+        System.out.println("Invalid"); 
+     } else if (personAge <=5){
+        System.out.println("Baby");
+     } else if (personAge <=11) {
+        System.out.println("Kid");  
+     } else if (personAge <=17) {
+        System.out.println("Teen"); 
+     } else if (personAge >=18 ) {
+        System.out.println("Adult");  
+        
+      
+               
+               }
+    }
+       
     }
     
-}
+
+    
+
